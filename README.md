fadc4881944cade877da72fa6e4f44e0106e8bef
timmens <tim@mensinger96.com>
2022-06-16 18:06:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Scipy 2022
+
+## Practical Numerical Optimization with estimagic, scipy and jaxopt
+
+> by Janos Gabler & Tim Mensinger
+
+## Contents
+
+1. [Installation](#installation)
+1. [Slides](#slides)
+1. [Practices](#practices)
+
+## Installation
+
+1. Install [miniconda](https://docs.conda.io/en/latest/miniconda.html)
+
+1. Clone the repository
+
+   ```console
+   $ git clone https://github.com/OpenSourceEconomics/scipy-pres.git
+   $ cd scipy-pres
+   ```
+
+1. Install and activate the environment
+
+   ```console
+   $ conda env create -f environment.yml
+   $ conda activate scipy_estimagic
+   ```
+
+   > **Note** You have to repeat the activation step each time after closing your
+   > terminal.
+
+1. Test your installation
+
+   ```console
+   $ pytask
+   ```
+
+## Slides
+
+Here we provide links and additional information of the slides.
+
+## Practices
+
+Here we provide links and additional information of the practice notebooks.
---
+++ test_installation.py
@@ -0,0 +1,88 @@
+"""Test that the environment is correctly installed."""
+from distutils.spawn import find_executable
+
+
+def is_installed(executable):
+    return bool(find_executable(executable))
+
+
+# ======================================================================================
+# Check Installations
+# ======================================================================================
+
+required_estimagic_version = "0.3.2"
+
+try:
+    import estimagic  # noqa: F401
+except ModuleNotFoundError:
+    msg = (
+        "\n\nestimagic is not installed. Please install it via "
+        f"'conda install -c conda-forge estimagic=={required_estimagic_version}'"
+    )
+    raise ModuleNotFoundError(msg)  # noqa: TC200
+
+from estimagic import __version__  # noqa: E402
+
+if required_estimagic_version not in __version__:
+    msg = (
+        f"You've installed estimagic version {__version__}, but we require version"
+        f"{required_estimagic_version}"
+    )
+    raise Exception(msg)  # noqa: TC002
+
+from estimagic.config import IS_DFOLS_INSTALLED  # noqa: E402
+from estimagic.config import IS_PYGMO_INSTALLED  # noqa: E402
+from estimagic.config import IS_PYBOBYQA_INSTALLED  # noqa: E402
+from estimagic.config import IS_NLOPT_INSTALLED  # noqa: E402
+from estimagic.config import IS_FIDES_INSTALLED  # noqa: E402
+from estimagic.config import IS_JAX_INSTALLED  # noqa: E402
+
+not_installed = []
+
+if not is_installed("jupyter"):
+    not_installed.append(("jupyter", "conda"))
+
+if not IS_NLOPT_INSTALLED:
+    not_installed.append(("nlopt", "conda"))
+
+if not IS_JAX_INSTALLED:
+    not_installed.append(("jax", "conda"))
+
+if not IS_PYGMO_INSTALLED:
+    not_installed.append(("pygmo", "conda"))
+
+if not IS_FIDES_INSTALLED:
+    not_installed.append(("fides", "pip"))
+
+if not IS_DFOLS_INSTALLED:
+    not_installed.append(("DFO-LS", "pip"))
+
+if not IS_PYBOBYQA_INSTALLED:
+    not_installed.append(("Py-BOBYQA", "pip"))
+
+try:
+    import jaxopt  # noqa: E402, F401
+except ModuleNotFoundError:
+    not_installed(("jaxopt", "pip"))
+
+
+# ======================================================================================
+# Raise Error
+# ======================================================================================
+
+not_installed_conda = [pkg for pkg, pkg_mng in not_installed if pkg_mng == "conda"]
+not_installed_pip = [pkg for pkg, pkg_mng in not_installed if pkg_mng == "pip"]
+
+
+msg = "The following packages are missing and need to be installed via:"
+
+if not_installed_conda:
+    msg += f"\nconda: {not_installed_conda}."
+
+if not_installed_pip:
+    msg += f"\npip: {not_installed_pip}."
+
+if not_installed_conda or not_installed_pip:
+    raise ModuleNotFoundError(msg)
+else:
+    print("\nYour installation is working!!!\n")  # noqa: T201
